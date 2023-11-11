425ef2d62b585817c4b5e293f3a0f5f4872761df
GurkirtSingh <GurkirtSingh143@gmail.com>
2023-11-10 18:57:52 -0800

    Created API
    Converted the script to flask app. API access to support web front end

--- bbo.py
+++ bbo.py
@@ -2,6 +2,7 @@ import sys
 import csv
 import constants
 import pyperclip
+import io
 
 AllOrigins = constants.AllOrigins
 OtherCanada = constants.OtherCanada
@@ -22,61 +23,61 @@ def getInputfilepath():
     mfr_path = input("\nFile path (Market Feeder Report): ")
     return mfr_path
 
-def main():
-    mfrPath = getInputfilepath()
+def SortByBookingOrigin(file):
+    
     try:
-        with open(mfrPath, newline='') as mfrFile:
-            mfrReader = csv.reader(mfrFile,delimiter=',', quotechar='|')
+        csv_contents = file.read().decode('utf-8', errors='replace')
+        mfrReader = csv.reader(io.StringIO(csv_contents),delimiter=',', quotechar='|')
 
-            # skip the first line that contain header
-            next(mfrReader)
-            # total
-            totalBookings = 0
-            #assign each states data to the dictionary
-            for line in mfrReader:
-                state = line[1]
-                bookings = int(line[2])
-                totalBookings += bookings
-                if state in AllOrigins:
-                    AllOrigins[state] += bookings
-                elif state in OtherCanada:
-                    AllOrigins["Canada Other"] += bookings
-                elif state in OtherMountainStates:
-                    AllOrigins["Other Mountain States"] += bookings
-                    AllOrigins["US Total"] += bookings
-                elif state in OtherSouthernStates:
-                    AllOrigins["Other Southern States"] += bookings
-                    AllOrigins["US Total"] += bookings
-                elif state in MidwestStates:
-                    AllOrigins["Midwest States"] += bookings
-                    AllOrigins["US Total"] += bookings
-                elif state in NewEnglandStates:
-                    AllOrigins["New England States"] += bookings
-                    AllOrigins["US Total"] += bookings
-                elif state in MiddleAtlantic:
-                    AllOrigins["Middle Atlantic"] += bookings
-                    AllOrigins["US Total"] += bookings
-                elif state in SouthAtlantic:
-                    AllOrigins["South Atlantic"] += bookings
-                    AllOrigins["US Total"] += bookings
-                elif state in HawaiiandAlaska:
-                    AllOrigins["Hawaii/Alaska"] += bookings
-                    AllOrigins["US Total"] += bookings
-                elif state in Australia:
-                    AllOrigins["Australia"] += bookings
-                elif state in Japan:
-                    AllOrigins["Japan"] += bookings
-                elif state in Mexico:
-                    AllOrigins["Mexico"] += bookings
-                    AllOrigins["Other Countries"] += bookings
-                elif state in UnitedKingdom:
-                    AllOrigins["United Kingdom"] += bookings
-                    AllOrigins["Europe Total"] += bookings
-                elif state in Brazil:
-                    AllOrigins["Brazil"] += bookings
-                    AllOrigins["Other Countries"] += bookings
-                else:
-                    AllOrigins["Other Unknown"] += bookings 
+        # skip the first line that contain header
+        next(mfrReader)
+        # total
+        totalBookings = 0
+        #assign each states data to the dictionary
+        for line in mfrReader:
+            state = line[1]
+            bookings = int(line[2])
+            totalBookings += bookings
+            if state in AllOrigins:
+                AllOrigins[state] += bookings
+            elif state in OtherCanada:
+                AllOrigins["Canada Other"] += bookings
+            elif state in OtherMountainStates:
+                AllOrigins["Other Mountain States"] += bookings
+                AllOrigins["US Total"] += bookings
+            elif state in OtherSouthernStates:
+                AllOrigins["Other Southern States"] += bookings
+                AllOrigins["US Total"] += bookings
+            elif state in MidwestStates:
+                AllOrigins["Midwest States"] += bookings
+                AllOrigins["US Total"] += bookings
+            elif state in NewEnglandStates:
+                AllOrigins["New England States"] += bookings
+                AllOrigins["US Total"] += bookings
+            elif state in MiddleAtlantic:
+                AllOrigins["Middle Atlantic"] += bookings
+                AllOrigins["US Total"] += bookings
+            elif state in SouthAtlantic:
+                AllOrigins["South Atlantic"] += bookings
+                AllOrigins["US Total"] += bookings
+            elif state in HawaiiandAlaska:
+                AllOrigins["Hawaii/Alaska"] += bookings
+                AllOrigins["US Total"] += bookings
+            elif state in Australia:
+                AllOrigins["Australia"] += bookings
+            elif state in Japan:
+                AllOrigins["Japan"] += bookings
+            elif state in Mexico:
+                AllOrigins["Mexico"] += bookings
+                AllOrigins["Other Countries"] += bookings
+            elif state in UnitedKingdom:
+                AllOrigins["United Kingdom"] += bookings
+                AllOrigins["Europe Total"] += bookings
+            elif state in Brazil:
+                AllOrigins["Brazil"] += bookings
+                AllOrigins["Other Countries"] += bookings
+            else:
+                AllOrigins["Other Unknown"] += bookings 
     
         # Canada tatal
         AllOrigins["Canada Total"] = AllOrigins["British Columbia"] + AllOrigins["Alberta"] + AllOrigins["Ontario"] + AllOrigins["Quebec"] + AllOrigins["Canada Other"]
@@ -93,15 +94,9 @@ def main():
         # Grand Total
         AllOrigins['Grand Total'] = totalBookings
     
-        # Copy to clipboard
-        pyperclip.copy('\n'.join([str(s) for s in AllOrigins.values()]))
+        return '\n'.join([str(s) for s in AllOrigins.values()])
+    except Exception as e: raise
         
-        print('\nSuccess: Results are copied to clipboard')
-
-    except IOError as e:
-        print(e)
-    except:
-        print('\nError: something is wrong!')
 
 def header():
     print(''.join('*' for i in range(43)))
@@ -113,5 +108,14 @@ def footer():
     input('\n\n***\tPress any key to exit the program.')
 if __name__ == "__main__":
     header()
-    main()
+    mfrPath = getInputfilepath()
+    try:
+        results = SortByBookingOrigin(mfrPath)
+        # Copy to clipboard
+        pyperclip.copy(results)
+        print('\nSuccess: Results are copied to clipboard')
+    except IOError as e:
+        print(e)
+    except:
+        print('\nError: something is wrong!')
     footer()
